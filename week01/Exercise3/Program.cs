c2afa6a440110d2b9d84d85ed34d966e0bbb1ad3
Bolaji Akilo <Bolajijohnson0007.com>
2025-05-18 11:19:03 -0700

    Exercise 3

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -3,7 +3,27 @@ using System;
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+     {
+        Random random = new Random();
+        int magicNumber = random.Next(1, 101); 
+
+        int guess;
+        do
+        {
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (guess < magicNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (guess > magicNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+
+        } while (guess != magicNumber);
+
+        Console.WriteLine("You guessed it!");
     }
 }
